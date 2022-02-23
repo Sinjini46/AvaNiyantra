e8600f1371c49bbee642a4f4b9b5e58b0ac59d4b
Kivuos <sarkar.souvik2016@outlook.com>
2022-02-23 23:28:34 +0530

    create account page backend

--- frontend/home/script.js
+++ frontend/home/script.js
@@ -1,9 +1,34 @@
-const nav=document.querySelector('.row');
-fetch('/check.html')
-.then(res=>res.text())
-.then(data=>{
-    nav.innerHTML=data
-    const parser=new DOMParser()
-    const doc=parser.parseFromString(data,'text/html')
-    eval(doc.querySelector('script').textContent)
+btn = document.getElementById('submit')
+
+btn.addEventListener('click', () => {
+
+    n = document.getElementById('name').value
+    c = document.getElementById('category').value
+    r = document.getElementById('registration').value
+    e = document.getElementById('email').value
+    p = document.getElementById('password-field').value
+    console.log(c)
+    user = {
+        "name": n,
+        "category": c,
+        "registration": r,
+        "email": e,
+        "password": p,
+    }
+
+    console.log(user)
+
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    }
+
+    let fetchRes = fetch(url = "https://sih2022.herokuapp.com/user/createuser", options);
+    fetchRes.then(res => res.json())
+        .then(d => {
+            console.log(d)
+        })
 })
