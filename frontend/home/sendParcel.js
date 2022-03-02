859609949c017d0d67d8a64c95f5b9391cd2c28c
Kivuos <sarkar.souvik2016@outlook.com>
2022-03-02 22:08:09 +0530

    slight modification

--- frontend/home/sendParcel.js
+++ frontend/home/sendParcel.js
@@ -32,24 +32,52 @@ btn.addEventListener("click", () => {
                 const gen_pk_id = fetch(url = "http://sih2022.herokuapp.com/parcel/add_parcel_id", options);
                 gen_pk_id.then(res => res.json())
                     .then(d => {
-                        const pk_id = d.data
 
-                        let opt = {
+                        let uopt = {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json;charset=utf-8'
                             },
                             body: JSON.stringify({
-                                "receiver_id": recv_id,
-                                "sender_id": sender_id,
-                                "parcel_id": pk_id
+                                _id: localStorage.getItem('user')
                             })
                         }
-                        const gen_batch = fetch(url = "http://sih2022.herokuapp.com/batch/create_batch", opt);
-                        gen_batch.then(res => res.json())
-                            .then(d => {
-                                console.log(d)
+                        const gen_name = fetch(url = "http://sih2022.herokuapp.com/user/get_user_by_id", uopt);
+                        gen_name.then(res => res.json())
+                            .then(uname => {
+                                const pk_id = d.data
+
+                                let opt = {
+                                    method: 'POST',
+                                    headers: {
+                                        'Content-Type': 'application/json;charset=utf-8'
+                                    },
+                                    body: JSON.stringify({
+                                        "receiver_id": recv_id,
+                                        "sender_id": sender_id,
+                                        "name": uname.data.name,
+                                        "parcel_id": pk_id
+                                    })
+                                }
+                                const gen_batch = fetch(url = "http://sih2022.herokuapp.com/batch/create_batch", opt);
+                                gen_batch.then(res => res.json())
+                                    .then(d => {
+                                        console.log(d)
+                                    })
                             })
+
+
+
+
+
+
+
+
+
+
+
+
+
                     })
 
 
--- frontend/manufacturer/assets/js/incomingorder.js
+++ frontend/manufacturer/assets/js/incomingorder.js
@@ -1,66 +1,47 @@
-user = {
-    "receiver_id": localStorage.getItem('user')
-
-}
-
 let options = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify(user)
+    body: JSON.stringify({ "receiver_id": localStorage.getItem('user') })
 }
 
 let fetchRes = fetch(url = "https://sih2022.herokuapp.com/batch/get_recv", options);
-fetchRes.then(res => res.json()).then(d => {
-    prev = []
-    container = document.getElementById("container")
-    for (i = 0; i < d.data.length; i++) {
-        a = d.data[i].parcel_id
-        if (!prev.includes(a)) {
-            prev.push(a)
-            sender_id = d.data[i].sender_id
+fetchRes
+    .then(async res => res.json())
+    .then(async d => {
+        arr = []
+        container = document.getElementById("container")
+        for (i = 0; i < d.data.length; i++) {
+            console.log(d)
+            a = d.data[i].parcel_id
+            if (!arr.includes(a)) {
+                arr.push(a)
+                n = d.data[i].sender_name
+                tn1 = document.createTextNode(a)
+                tn0 = document.createTextNode(i + 1)
+                tn2 = document.createTextNode(n)
 
-            tn1 = document.createTextNode(d.data[i].parcel_id)
-            sender = {
-                "_id": sender_id
+                td0 = document.createElement("td")
+                td0.appendChild(tn0)
+
+                anchor = document.createElement("a")
+                anchor.setAttribute("href", a);
+                anchor.appendChild(tn1)
+                td1 = document.createElement("td")
+                td1.appendChild(anchor)
+
+
+                td2 = document.createElement("td")
+                td2.appendChild(tn2)
+
+                tr1 = document.createElement("tr")
+
+                tr1.appendChild(td0)
+                tr1.appendChild(td2)
+                tr1.appendChild(td1)
+
+                container.appendChild(tr1)
             }
-            let options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                body: JSON.stringify(sender)
-            }
-
-            let userData = fetch(url = "https://sih2022.herokuapp.com/user/get_user_by_id", options);
-            userData.then(res => res.json())
-                .then(res => {
-                    n = res.data.name
-                    tn0 = document.createTextNode(i)
-                    tn2 = document.createTextNode(n)
-
-                    td0 = document.createElement("td")
-                    td0.appendChild(tn0)
-
-                    anchor = document.createElement("a")
-                    anchor.setAttribute("href", a);
-                    anchor.appendChild(tn1)
-                    td1 = document.createElement("td")
-                    td1.appendChild(anchor)
-
-
-                    td2 = document.createElement("td")
-                    td2.appendChild(tn2)
-
-                    tr1 = document.createElement("tr")
-
-                    tr1.appendChild(td0)
-                    tr1.appendChild(td2)
-                    tr1.appendChild(td1)
-
-                    container.appendChild(tr1)
-                })
         }
-    }
-})
+    })
